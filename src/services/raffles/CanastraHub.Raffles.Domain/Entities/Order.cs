6625ec3cbe8b6c7801a11aca1d3f00b0323d0ad9
Rafael Freitas <raffreitasx@outlook.com>
2024-05-30 09:49:37 -0300

    feat: add order aggregate

---
+++ src/services/raffles/CanastraHub.Raffles.Domain/Entities/Order.cs
@@ -0,0 +1,53 @@
+﻿using CanastraHub.Core.DomainObjects;
+using CanastraHub.Raffles.Domain.Enums;
+
+namespace CanastraHub.Raffles.Domain.Entities;
+public class Order : Entity, IAggregateRoot
+{
+    public Guid RaffleId { get; private set; }
+    public Guid ParticipantId { get; private set; }
+    public decimal TotalValue { get; private set; }
+    public OrderStatus OrderStatus { get; private set; }
+
+    private readonly IList<Quota> _quotas = [];
+    public IReadOnlyCollection<Quota> Quotas => _quotas.AsReadOnly();
+
+    public Order(Guid raffleId, Guid participantId)
+    {
+        RaffleId = raffleId;
+        ParticipantId = participantId;
+        OrderStatus = OrderStatus.Created;
+    }
+
+    private void CalculateTotal()
+    {
+        TotalValue = Quotas.Sum(q => q.Value);
+    }
+
+    public void AddQuota(Quota quota)
+    {
+        quota.AssignToOrder(Id);
+
+        _quotas.Add(quota);
+
+        CalculateTotal();
+    }
+
+    public void MarkAsPaid()
+    {
+        OrderStatus = OrderStatus.Paid;
+        foreach (var quota in Quotas)
+        {
+            quota.MarkAsPaid();
+        }
+    }
+
+    public void MarkAsRefunded()
+    {
+        OrderStatus = OrderStatus.Refunded;
+        foreach (var quota in Quotas)
+        {
+            quota.MarkAsRefunded();
+        }
+    }
+}
---
+++ src/services/raffles/CanastraHub.Raffles.Domain/Enums/OrderStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace CanastraHub.Raffles.Domain.Enums;
+public enum OrderStatus : byte
+{
+    Created = 1,
+    Paid = 2,
+    Refunded = 3
+}
