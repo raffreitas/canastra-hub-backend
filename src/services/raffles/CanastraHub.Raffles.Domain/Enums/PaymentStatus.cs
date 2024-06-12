bda56cc904df90a05e003073c8001d1cf73fbcc2
Rafael Freitas <raffreitasx@outlook.com>
2024-06-11 21:24:02 -0300

    feat: removed raffle service

--- src/services/raffles/CanastraHub.Raffles.Domain/Enums/PaymentStatus.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CanastraHub.Raffles.Domain.Enums;
-public enum PaymentStatus : byte
-{
-    Pending = 1,
-    Paid = 2,
-    Refunded = 3
-}
--- src/services/raffles/CanastraHub.Raffles.Domain/Services/IRaffleService.cs
+++
@@ -1,7 +0,0 @@
-﻿using CanastraHub.Raffles.Domain.Entities;
-
-namespace CanastraHub.Raffles.Domain.Services;
-public interface IRaffleService
-{
-    IList<Quota> CreateQuotas(Raffle raffle, Participant participant, int quantity, HashSet<int> usedNumbers);
-}
